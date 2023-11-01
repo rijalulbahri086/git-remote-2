8cc70a332b4b763c90826c3fc14ba10285ef19a5
unknown <rijalulbahri086@gmail.com>
2023-11-01 09:16:40 +0700

    menambahkan h2

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 </head>
 <body>
     <h1>hello world</h1>
+    <h2>hello</h2>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, reprehenderit non repudiandae, illo modi quae debitis asperiores accusamus magnam magni fugit quos dignissimos aperiam nulla sequi. Quos repellendus ipsam nam?</p>
 </body>
 </html>
